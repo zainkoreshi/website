32c3309fe73c51e988755f933eafed09b701d190
Zain Koreshi <zainkoreshi769@gmail.com>
2023-12-25 01:23:25 +0800

    first push

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -69,7 +69,7 @@ const Education = () => {
           </div>
 
           {/* gradient start */}
-          <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+          <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white" />
           <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
           {/* gradient end */}
         </div>
