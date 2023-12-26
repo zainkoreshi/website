7dd00dbb6f77afd0acaeb4f43574048d5d9a889c
Zain Koreshi <zainkoreshi769@gmail.com>
2023-12-26 13:33:02 +0800

    loading push

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,7 +4,7 @@ import binance from "./binance.png";
 import card from "./card.png";
 import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
-import parthmittal from "./parth.svg";
+import parthmittal from "./parth.png";
 import profilePic from "./profile-pic.jpg";
 import quotes from "./quotes.svg";
 import robot from "./robot.png";
