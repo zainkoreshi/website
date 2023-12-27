a96f989c06b4b62d2be7a543429dd8261fb93cf2
Zain Koreshi <zainkoreshi769@gmail.com>
2023-12-27 16:45:04 +0800

    edit patch 7

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => (
     </div>
     <div className="text-center font-poppins font-normal text-dimWhite text-xs sm:text-sm pb-4">
       <p>
-        Thank you so much for visiting! I really appreciate it.
+        Thank you so much for visiting!
       </p>
     </div>
   </footer>
